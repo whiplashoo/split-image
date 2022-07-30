db01fb135e5f2bcdd8a343f908645ede2a7ce75f
minas-mbp <whiplashoo721@gmail.com>
2022-07-30 20:59:36 +0300

    chore: update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="split-image",
-    version="1.1.2",
+    version="1.1.3",
     author="Minas Giannekas",
     author_email="acontact@whidev.com",
     description="A package that lets you quickly split an image into rows and columns (tiles).",
