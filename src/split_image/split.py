a13449b8f0708cfb56af3707251292ec8080e1a1
Minas Giannekas <whiplashoo721@gmail.com>
2022-11-03 18:42:49 +0200

    fix: use correct export mode for squared images

--- src/split_image/split.py
+++ src/split_image/split.py
@@ -12,20 +12,29 @@ def split_image(image_path, rows, cols, should_square, should_cleanup, output_di
     im_width, im_height = im.size
     row_width = int(im_width / rows)
     row_height = int(im_height / cols)
+    name, ext = os.path.splitext(image_path)
+    if output_dir != None:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+    else:
+        output_dir = "./"
     if should_square:
         min_dimension = min(im_width, im_height)
         max_dimension = max(im_width, im_height)
         print("Resizing image to a square...")
         bg_color = determine_bg_color(im)
         print("Background color is... " + str(bg_color))
-        im_r = Image.new("RGBA", (max_dimension, max_dimension), bg_color)
+        im_r = Image.new("RGBA" if ext == "png" else "RGB",
+                         (max_dimension, max_dimension), bg_color)
         offset = int((max_dimension - min_dimension) / 2)
         if im_width > im_height:
             im_r.paste(im, (0, offset))
         else:
             im_r.paste(im, (offset, 0))
         print("Exporting resized image...")
-        im_r.save(image_path + "_squared.png")
+        outp_path = name + "_squared" + ext
+        outp_path = os.path.join(output_dir, outp_path)
+        im_r.save(outp_path)
         im = im_r
     n = 0
     for i in range(0, cols):
@@ -33,12 +42,8 @@ def split_image(image_path, rows, cols, should_square, should_cleanup, output_di
             box = (j * row_width, i * row_height, j * row_width +
                    row_width, i * row_height + row_height)
             outp = im.crop(box)
-            name, ext = os.path.splitext(image_path)
             outp_path = name + "_" + str(n) + ext
-            if output_dir != None:
-                if not os.path.exists(output_dir):
-                    os.makedirs(output_dir)
-                outp_path = os.path.join(output_dir, outp_path)
+            outp_path = os.path.join(output_dir, outp_path)
             print("Exporting image tile: " + outp_path)
             outp.save(outp_path)
             n += 1
